9624f434c745631ca3196653ff576d39d10b19d3
Dana indra <81427836+tesastor25@users.noreply.github.com>
2021-09-21 21:23:10 +0700

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,4 +1,6 @@
 # Install Datadog into Kubernetes using Helm chart
+
+# First install helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
