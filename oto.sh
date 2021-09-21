53459629475c4fa4432d6d20c0f9db4adfe6d729
Dana indra <81427836+tesastor25@users.noreply.github.com>
2021-09-21 21:18:52 +0700

    Update oto.sh

--- oto.sh
+++ oto.sh
@@ -1,6 +1,6 @@
 # Install Datadog into Kubernetes using Helm chart
 
-API_KEY="9e4776dfc76fd4512a9bac817c2bc45a"
+API_KEY="insertyourapikeyfromdatadog"
 
 #helm repo add datadog https://helm.datadoghq.com
 
